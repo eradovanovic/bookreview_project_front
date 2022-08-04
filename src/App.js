75379cfcc2cb6b75855494318b556f5249052004
Emilija Radovanovic <emilija@77-46-141-236.static.isp.telekom.rs>
2022-08-04 16:27:07 +0200

    Added new routes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import {Route, Routes} from "react-router-dom";
 import "./App.css";
 import configureStore from "./store/index";
 import Home from "./pages/Home/Home";
-import Action from "./pages/Action/Action";
-import Login from "./pages/Login/Login";
-import MainHeader from "./components/Layout/MainHeader/MainHeader";
-import Register from "./pages/Register/Register";
-import Books from "./pages/Books/Books";
-import Authors from "./pages/Authors/Authors";
-import Search from "./pages/Search/Search";
-import BookCollection from "./pages/BookCollection/BookCollection";
-import NewBook from "./pages/NewBook/NewBook";
-import NewAuthor from "./pages/NewAuthor/NewAuthor";
-import ChangePassword from "./pages/ChangePassword/ChangePassword";
-import NotFound from "./pages/NotFound/NotFound";
+import Action from "./pages/Action";
+import Login from "./pages/Login";
+import MainHeader from "./components/Layout/MainHeader";
+import Register from "./pages/Register";
+import Books from "./pages/Books";
+import Authors from "./pages/Authors";
+import Search from "./pages/Search";
+import BookCollection from "./pages/BookCollection";
+import NewBook from "./pages/NewBook";
+import NewAuthor from "./pages/NewAuthor";
+import ChangePassword from "./pages/ChangePassword";
+import NotFound from "./pages/NotFound";
+import BookDetails from "./pages/BookDetails";
+import AuthorDetails from "./pages/AuthorDetails";
 
 
 const store = configureStore();
@@ -30,7 +32,9 @@ const App = (props) => {
                 <Route path='/' element={<MainHeader/>}>
                     <Route path='/' element={<Home/>}/>
                     <Route path='/books' element={<Books/>}/>
+                    <Route path='/bookDetails' element={<BookDetails/>}/>
                     <Route path='/authors' element={<Authors/>}/>
+                    <Route path='/authorDetails' element={<AuthorDetails/>}/>
                     <Route path='/search' element={<Search/>}/>
                     <Route path='/collection' element={<BookCollection/>}/>
                     <Route path='/newBook' element={<NewBook/>}/>
