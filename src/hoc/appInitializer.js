36d1f4f88912cee691924dcc65a83feae2d4ef23
Emilija Radovanovic <emilija@77-46-141-236.static.isp.telekom.rs>
2022-09-02 12:29:01 +0200

    Fixed app initializer

--- src/hoc/appInitializer.js
+++ src/hoc/appInitializer.js
@@ -4,24 +4,23 @@ import {loginSuccess} from "../store/auth/authActions";
 import LoadingScreen from "../components/Layout/LoadingScreen";
 
 const appInitializer = WrappedComponent => props => {
-    const [loading, setLoading] = useState(false);
+    const [loaded, setLoaded] = useState(false);
     const {user} = useSelector(state => state.authReducer);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        setLoading(true);
         const localStorageUser = JSON.parse(localStorage.getItem("user"));
         const localStorageToken = localStorage.getItem("token");
         if(!user && localStorageUser){
             dispatch(loginSuccess({'user': localStorageUser, "token": localStorageToken}));
         }
         setTimeout(() => {
-            setLoading(false);
+            setLoaded(true);
         }, 1000);
 
     }, []);
 
-    return !loading ? <WrappedComponent {...props} /> : <LoadingScreen/>
+    return loaded ? <WrappedComponent {...props} /> : <LoadingScreen/>
 }
 
 export default appInitializer;
