d506afb8ffe2c5d49d7d3ea7a4c86dda3601d62a
Emilija Radovanovic <emilija@77-46-141-236.static.isp.telekom.rs>
2023-01-24 15:27:24 +0100

    Code fix

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,108 +1,10 @@
-import {AUTHORS_PER_PAGE, BOOKS_PER_PAGE, DEFAULT_AVATAR_PHOTO, DEFAULT_BOOK_PHOTO, SORT} from "constants/constants";
+import {AUTHORS_PER_PAGE, BOOKS_PER_PAGE} from "constants/constants";
 import axios from "services/axios.instance";
 import {sortHandler} from "utils/sortMapping";
 
-import {mockBooks, mockCollection, mockReviews} from "./mockData";
-import {mockAuthors} from "./mockData";
-
-// const bookDetailsInForm = data => {
-//     return {
-//         id: data.key,
-//         photo: `https://covers.openlibrary.org/b/id/${data.details.covers?.[0]}-L.jpg`,
-//         author_id: data.details.authors?.[0].key,
-//         author: data.details.authors?.[0].name,
-//         title: data.details.title,
-//         description: data.details.description
-//     }
-// }
-
-// const bookDetailsInForm = data => {
-//     return {
-//         id: data.key.replace('/works','/books'),
-//         photo: `https://covers.openlibrary.org/b/id/${data.covers?.[0]}-L.jpg`,
-//         author_id: data.authors?.[0].author.key.replace('/authors/', ''),
-//         // author: fetchAuthor(data.authors?.[0].author.key.replace('/authors','')).name,
-//         title: data.title,
-//         description: data.description.value ? data.description.value : data.description,
-//     }
-// }
-//
-// const bookByAuthorInForm = data => {
-//     return {
-//         id: data.key.replace('/works','/books'),
-//         photo: `https://covers.openlibrary.org/b/id/${data.covers?.[0]}-L.jpg`,
-//         // author_id: data.details.authors?.[0].key,
-//         // author: data.details.authors?.[0].name,
-//         title: data.title,
-//     }
-// }
-
-// const bookInForm = data => {
-//     return {
-//         photo: `https://covers.openlibrary.org/b/id/${data.cover_i}-L.jpg`,
-//         id: data.key.replace('/works', '/books'),
-//         author_id: data.author_key.toString(),
-//         author: data.author_name.toString(),
-//         title: data.title,
-//         genres: [],
-//     }
-// }
-//
-// const booksByAuthorInForm = data => {
-//    const books = []
-//     data.entries.forEach(d => {
-//         books.push(bookByAuthorInForm(d))
-//     })
-//     return {total: data.size, books}
-// }
-//
-// const booksInForm = data => {
-//     const books = []
-//     data.docs.forEach(d => {
-//         books.push(bookInForm(d))
-//     })
-//     return {total: data.numFound, books}
-// }
-//
-// const authorDetailsInForm = data => {
-//     return {
-//         photo: data.photos?.[0] && `https://covers.openlibrary.org/a/id/${data.photos[0]}-L.jpg`,
-//         name: data.name,
-//         biography: data.bio?.value ? data.bio.value : data.bio,
-//     }
-// }
-//
-// const authorsInForm = data => {
-//     const authors = []
-//     data.docs.forEach(d => {
-//         authors.push(authorInForm(d))
-//     })
-//     return {total: data.numFound, authors}
-// }
-//
-// const authorInForm = data => {
-//     return {
-//         id: data.key,
-//         photo: `https://covers.openlibrary.org/a/id/${data.name}-L.jpg`,
-//         name: data.name,
-//         bookNum: data.work_count,
-//     }
-// }
 
 const getBookById = id => {
     return axios.get(`books/${id}`)
-
-    // mock data
-    // return fetch(`https://openlibrary.org/works/${id}.json`)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         const bookDetails = bookDetailsInForm(data)
-    //         return bookDetails
-    //     })
-    // const book = mockBooks.find(b => b.id === id);
-    // return new Promise((res, rej) => {
-    //     res(book);
-    // })
 }
 
 const getBooks = (page, genres, sortBy) => {
@@ -114,48 +16,36 @@ const getBooks = (page, genres, sortBy) => {
         sortBy: sort.sortBy,
         order: sort.order
     })
-
-    // mock data
-    // let books = [...mockBooks];
-    // const booksResult = filterAndSortBooks(books, page, genres, sortBy);
-    // return new Promise((res, rej) => {
-    //     res(booksResult);
-    // })
-    // // return fetch(`http://openlibrary.org/search.json?q=title:* subject:large_type_books&page=${page}&limit=${BOOKS_PER_PAGE}`)
-    // //     .then(response => response.json())
-    // //     .then(data => booksInForm(data))
 }
 
-const filterAndSortBooks = (books, page, genres, sortBy) => {
-    let filteredBooks = [...books];
-    if (genres && genres.length) {
-        filteredBooks = books.filter((book) => {
-            return book.genres.map(genre => genre.name).some(g => genres.includes(g));
-        });
-    }
-
-    switch (sortBy) {
-        case SORT.TITLE_ASC:
-            filteredBooks.sort((a,b) => a.title.localeCompare(b.title));
-            break;
-        case SORT.TITLE_DESC:
-            filteredBooks.sort((a,b) => b.title.localeCompare(a.title));
-            break;
-        case SORT.RATING_ASC:
-            filteredBooks.sort((a,b) => { return a.rating - b.rating;});
-            break;
-        case SORT.RATING_DESC:
-            filteredBooks.sort((a,b) => { return b.rating - a.rating;});
-            break;
-        default:
-            break;
-    }
-    return {books: filteredBooks.slice((page - 1) * BOOKS_PER_PAGE, page * BOOKS_PER_PAGE), total: filteredBooks.length};
-}
+// const filterAndSortBooks = (books, page, genres, sortBy) => {
+//     let filteredBooks = [...books];
+//     if (genres && genres.length) {
+//         filteredBooks = books.filter((book) => {
+//             return book.genres.map(genre => genre.name).some(g => genres.includes(g));
+//         });
+//     }
+//
+//     switch (sortBy) {
+//         case SORT.TITLE_ASC:
+//             filteredBooks.sort((a,b) => a.title.localeCompare(b.title));
+//             break;
+//         case SORT.TITLE_DESC:
+//             filteredBooks.sort((a,b) => b.title.localeCompare(a.title));
+//             break;
+//         case SORT.RATING_ASC:
+//             filteredBooks.sort((a,b) => { return a.rating - b.rating;});
+//             break;
+//         case SORT.RATING_DESC:
+//             filteredBooks.sort((a,b) => { return b.rating - a.rating;});
+//             break;
+//         default:
+//             break;
+//     }
+//     return {books: filteredBooks.slice((page - 1) * BOOKS_PER_PAGE, page * BOOKS_PER_PAGE), total: filteredBooks.length};
+// }
 
 const addBook = (title, author_id, genres, description, photo) => {
-    // const photoSth = photo.toDataURL()
-
     return axios.post('/books/addBook', {
         title,
         authorID: author_id,
@@ -167,45 +57,14 @@ const addBook = (title, author_id, genres, description, photo) => {
         description,
         photo
     })
-
-    // mock data
-    // const id = [...mockBooks].sort((r1, r2) => r2.id - r1.id).at(0).id + 1;
-    // const bookObj = {
-    //     id: id,
-    //     title: title,
-    //     author_id: author_id,
-    //     author: author,
-    //     genres: genres,
-    //     rating: 0,
-    //     numberOfReviews: 0,
-    //     description: description,
-    //     photo: photo ? photo.name : DEFAULT_BOOK_PHOTO,
-    //     photoFile: photo ? URL.createObjectURL(photo) : null,
-    // }
-    // mockBooks.push(bookObj);
-    // return new Promise((res, rej) => {
-    //     res(bookObj);
-    // });
 }
 
 const getReviews = book_id => {
     return axios.get(`/reviews/book/${book_id}`)
-
-    // mock data
-    // return new Promise((res, rej) => {
-    //     res(mockReviews.filter(review => review.book_id === book_id)
-    //         .sort((r1, r2) => new Date(r2.date_reviewed) - new Date(r1.date_reviewed)));
-    // });
 }
 
 const getReviewsUser = username => {
     return axios.get(`/reviews/user/${username}`)
-
-    // mock data
-    // return new Promise((res, rej) => {
-    //     res(mockReviews.filter(review => review.user === username)
-    //         .sort((r1, r2) => new Date(r2.date_reviewed) - new Date(r1.date_reviewed)));
-    // });
 }
 
 const checkIfReviewed = (user, book_id) => {
@@ -213,12 +72,6 @@ const checkIfReviewed = (user, book_id) => {
         username: user,
         book_id
     })
-
-    // mock data
-    // const review = mockReviews.find(r => r.book_id === book_id && r.user === user);
-    // return new Promise((res, rej) => {
-    //     res(review);
-    // });
 }
 
 const addReview = (user, book_id, rating, review) => {
@@ -228,43 +81,10 @@ const addReview = (user, book_id, rating, review) => {
         rating,
         review,
     })
-   //  const review_id = mockReviews.sort((r1, r2) => r2.id - r1.id).at(0).id + 1;
-   //  const reviewObj = {
-   //     id: review_id,
-   //     user: user,
-   //     avatar: avatar,
-   //     book_id: book_id,
-   //     title: title,
-   //     date_reviewed: date_reviewed,
-   //     rating: rating,
-   //     review: review
-   //  }
-   // mockReviews.push(reviewObj);
-   //
-   // const index = mockBooks.findIndex(book => book.id === book_id);
-   // mockBooks[index].rating = (mockBooks[index].rating * mockBooks[index].numberOfReviews + rating) /  (mockBooks[index].numberOfReviews + 1);
-   // mockBooks[index].numberOfReviews++;
-   //
-   // return new Promise((res, rej) => {
-   //     res(reviewObj);
-   // });
 }
 
 const deleteReview = review_id => {
     return axios.delete(`/reviews/${review_id}`)
-    // const index = mockReviews.findIndex(review => review.id === review_id);
-    // const book_index = mockBooks.findIndex(book => book.id === mockReviews[index].book_id);
-    //
-    // if (mockBooks[book_index].numberOfReviews > 1) {
-    //     mockBooks[book_index].rating = (mockBooks[book_index].rating * mockBooks[book_index].numberOfReviews - mockReviews[index].rating) / (mockBooks[book_index].numberOfReviews - 1);
-    // }
-    // else {
-    //     mockBooks[book_index].rating = 0;
-    // }
-    //
-    // mockBooks[book_index].numberOfReviews--;
-    // mockReviews.splice(index, 1);
-    // return new Promise((res, rej) => res(mockReviews));
 }
 
 const getCollectionForUser = (username, page, genres, sortBy) => {
@@ -276,9 +96,6 @@ const getCollectionForUser = (username, page, genres, sortBy) => {
         sortBy: sort.sortBy,
         order: sort.order
     })
-    // const books = mockCollection.map(item => item.book);
-    // const booksInCollection = filterAndSortBooks(books, page, genres, sortBy);
-    // return new Promise((res, rej) => res(booksInCollection));
 }
 
 const checkBookInCollection = (username, book_id) => {
@@ -286,8 +103,6 @@ const checkBookInCollection = (username, book_id) => {
         username,
         book_id,
     })
-    // const books = mockCollection.filter(item => item.username === username && item.book.id === book_id);
-    // return new Promise((res, rej) => res(books));
 }
 
 const addBookToCollection = (book_id, title, author, genres, rating, numberOfReviews, photo, description, username ) => {
@@ -295,44 +110,14 @@ const addBookToCollection = (book_id, title, author, genres, rating, numberOfRev
         username,
         book_id,
     })
-    // const collection_item = {
-    //     book: {
-    //         id: book_id,
-    //         title: title,
-    //         author: author,
-    //         genres: genres,
-    //         rating: rating,
-    //         numberOfReviews: numberOfReviews,
-    //         description: description,
-    //         photo: photo,
-    //     },
-    //     username: username
-    // }
-    // mockCollection.push(collection_item);
-    // return new Promise((res, rej) => res(mockCollection));
 }
 
 const removeBookFromCollection = (username, book_id) => {
     return axios.delete(`/collections/${book_id}`)
-
-    //mock data
-    // const index = mockCollection.findIndex(item => item.username === username && item.book.id === book_id);
-    // mockCollection.splice(index, 1);
-    // return new Promise((res, rej) => res(mockCollection));
 }
 
 const getAuthorById = id => {
     return axios.get(`/authors/${id}`)
-    // mock data
-    // const author = mockAuthors.find(a => a.id === id);
-    // return new Promise((res, rej) => {
-    //     res(author ? author : mockAuthors[0]);
-    // })
-
-    //open library
-    // return fetch(` https://openlibrary.org/authors/${id}.json`)
-    //     .then(response => response.json())
-    //     .then(data => authorDetailsInForm(data))
 }
 
 const getAuthors = page => {
@@ -340,18 +125,6 @@ const getAuthors = page => {
         page,
         authorsPerPage: AUTHORS_PER_PAGE,
     })
-
-    // mock data
-    // return new Promise((res, rej) => {
-    //     res({authors: mockAuthors.slice((page - 1) * AUTHORS_PER_PAGE, page * AUTHORS_PER_PAGE), total: mockAuthors.length});
-    // })
-
-    //open library
-    // return fetch(`http://openlibrary.org/search.json?q=title:* subject:large_type_books&page=${page}&limit=${BOOKS_PER_PAGE}`)
-    // console.log(page)
-    // return fetch(`http://openlibrary.org/search/authors.json?&q=top_subjects:("Large type books")&offset=${(page - 1) * AUTHORS_PER_PAGE}&limit=${AUTHORS_PER_PAGE}&sort:["name asc"]`)
-    //     .then(response => response.json())
-    //     .then(data => authorsInForm(data))
 }
 
 const getAllAuthors = () => {
@@ -359,11 +132,6 @@ const getAllAuthors = () => {
         page: 1,
         authorsPerPage: Number.MAX_SAFE_INTEGER
     })
-
-    // mock data
-    // return new Promise((res, rej) => {
-    //     res({authors: mockAuthors});
-    // })
 }
 
 const addAuthor = (name, surname, biography, photo) => {
@@ -373,21 +141,6 @@ const addAuthor = (name, surname, biography, photo) => {
         biography,
         photo
     })
-    // mock data
-    // const id = [...mockAuthors].sort((r1, r2) => r2.id - r1.id).at(0).id + 1;
-    // const authorObj = {
-    //     id: id,
-    //     name: name,
-    //     surname: surname,
-    //     biography: biography,
-    //     photo: photo ? photo.name : DEFAULT_AVATAR_PHOTO,
-    //     photoFile: photo ? URL.createObjectURL(photo) : null,
-    //     bookNum: 0,
-    // }
-    // mockAuthors.push(authorObj);
-    // return new Promise((res, rej) => {
-    //     res(authorObj);
-    // });
 }
 
 
@@ -396,16 +149,6 @@ const getBooksByAuthor = (id, page) => {
         page,
         booksPerPage: BOOKS_PER_PAGE
     })
-    // mock data
-    // const books = mockBooks.filter(b => b.author_id === id);
-    // return new Promise((res, rej) => {
-    //     res({books: books.slice((page - 1) * BOOKS_PER_PAGE, page * BOOKS_PER_PAGE), total: books.length});
-    // })
-
-    // open library
-    // return fetch(`https://openlibrary.org/authors/${id}/works.json?limit=${BOOKS_PER_PAGE}&offset=${(page - 1) * BOOKS_PER_PAGE}`)
-    //     .then(response => response.json())
-    //     .then(data => booksByAuthorInForm(data))
 }
 
 const changeAuthorData = (id, name, surname, photo, biography) => {
@@ -415,16 +158,6 @@ const changeAuthorData = (id, name, surname, photo, biography) => {
         photo,
         biography
     })
-    // mock data
-    // const index = mockAuthors.findIndex(a => a.id === id)
-    // mockAuthors[index].name = name;
-    // mockAuthors[index].surname = surname;
-    // mockAuthors[index].photo = photo ? photo.name : mockAuthors[index].photo;
-    // mockAuthors[index].photoFile = photo ? URL.createObjectURL(photo) : null;
-    // mockAuthors[index].biography = biography;
-    // return new Promise((res, rej) => {
-    //     res(mockAuthors[index]);
-    // })
 }
 
 const search = (input) => {
@@ -433,12 +166,6 @@ const search = (input) => {
         page: 1,
         booksPerPage: Number.MAX_SAFE_INTEGER
     })
-    // mock data
-    // if (!input || input.trim() === "") {
-    //     return new Promise((res, rej) => res([]));
-    // }
-    // const books = mockBooks.filter(book => book.title.toLowerCase().includes(input.toLowerCase()));
-    // return new Promise((res, rej) => res(books));
 }
 
 const searchPagination = (input, page) => {
@@ -447,8 +174,6 @@ const searchPagination = (input, page) => {
         page: page,
         booksPerPage: BOOKS_PER_PAGE
     })
-    // const books = mockBooks.filter(book => book.title.toLowerCase().includes(input.toLowerCase()));
-    // return new Promise((res, rej) => res({books: books.slice((page - 1) * BOOKS_PER_PAGE, page * BOOKS_PER_PAGE), total: books.length}));
 }
 
 const getMostReviewedBooks = () => {
@@ -458,8 +183,6 @@ const getMostReviewedBooks = () => {
         sortBy: 'reviews',
         order: 'desc',
     })
-    // mock data
-    // return new Promise((res, rej) => res([...mockBooks].sort((b1, b2) => b2.numberOfReviews - b1.numberOfReviews).slice(0, 5)));
 }
 
 const getNewestAddedBooks = () => {
@@ -469,14 +192,10 @@ const getNewestAddedBooks = () => {
         sortBy: 'date_published',
         order: 'desc',
     })
-
-    // mock data
-    // return new Promise((res, rej) => res([...mockBooks].sort((b1, b2) => b2.id - b1.id).slice(0, 5)));
 }
 
 const getLatestReviews = () => {
     return axios.get('reviews/newest')
-    // return new Promise((res, rej) => res([...mockReviews].sort((r1, r2) => new Date(r2.date_reviewed) - new Date(r1.date_reviewed)).slice(0, 5)));
 }
 
 const getGenres = () => {
@@ -485,7 +204,6 @@ const getGenres = () => {
 
 const uploadPhoto = (imgFile) => {
     const formData= new FormData();
-    // formData.append('name', name);
     formData.append('image',imgFile);
     return fetch('https://api.imgbb.com/1/upload?key=054e02d81609aa2bba6579bb124c2202', {
         method: 'POST',
--- src/services/api/api_auth.js
+++ src/services/api/api_auth.js
@@ -1,20 +1,7 @@
 import { DEFAULT_AVATAR_PHOTO } from "constants/constants";
 import axios from "services/axios.instance";
 
-import { mockUsers } from "./mockAuth";
 import {setAccessToken} from "../common";
-
-const userInfoObject = (user) => {
-    return {
-        name: user.name,
-        surname: user.surname,
-        photo: user.photo,
-        photoFile: user.photoFile,
-        email: user.email,
-        username: user.username,
-        type: user.type
-    }
-}
 
 const login = (username, password) => {
     return axios.post('/login', {
@@ -25,19 +12,6 @@ const login = (username, password) => {
             setAccessToken(res.token)
             return res
         })
-    // const userRes = mockUsers.find(u => u.username === username && u.password === password);
-    // if (userRes){
-    //     return new Promise((res, rej) => {
-    //         res({
-    //             user: userInfoObject(userRes),
-    //             token: 'fake token'});
-    //     })
-    // }
-    // else {
-    //     const error = new Error('Wrong username or password!');
-    //     error.code = 401;
-    //     return Promise.reject(error);
-    // }
 }
 
 const register = (name, surname, email, username, password) => {
@@ -49,44 +23,10 @@ const register = (name, surname, email, username, password) => {
         username: username,
         password: password,
     })
-    // const userRes = mockUsers.find(u => u.username === username || u.email === email);
-    // if(userRes){
-    //     const error = new Error('Username or email is already in use!');
-    //     error.code = 409;
-    //     return Promise.reject(error);
-    // }
-    // else {
-    //     const newUser = {
-    //         name: name,
-    //         surname: surname,
-    //         email: email,
-    //         photo: DEFAULT_AVATAR_PHOTO,
-    //         username: username,
-    //         password: password,
-    //         type: 'user'
-    //     };
-    //     mockUsers.push(newUser);
-    //     return new Promise((res, rej) => {
-    //         res({
-    //                 user: userInfoObject(newUser),
-    //                 token: 'fake token'
-    //             });
-    //     })
-    // }
 }
 
 const getUserByUsername = username => {
     return axios.get(`/users/${username}`)
-    // const user = mockUsers.find(user => user.username === username);
-    // if (user) {
-    //     return new Promise((res, rej) => {
-    //         res(userInfoObject(user));
-    // }
-    // else {
-    //     return new Promise((res, rej) => {
-    //         res(user);
-    //     })
-    // }
 }
 
 const getLoggedUser = () => {
@@ -101,23 +41,6 @@ const updateUser = (username, name, surname, email, photo) => {
         email,
         photo,
     })
-    // const index = mockUsers.findIndex(u => u.username === username);
-    // const user = mockUsers.find(u => email !== mockUsers[index].email && u.email === email);
-    // if (!user) {
-    //     mockUsers[index].name = name;
-    //     mockUsers[index].surname = surname;
-    //     mockUsers[index].photo = photo ? photo.name : mockUsers[index].photo;
-    //     mockUsers[index].photoFile = photo ? URL.createObjectURL(photo) : null;
-    //     mockUsers[index].email = email;
-    //     return new Promise((res, rej) => {
-    //         res(userInfoObject(mockUsers[index]));
-    //     })
-    // }
-    // else {
-    //     const error = new Error("This mail is already in use!")
-    //     error.code = 409;
-    //     return Promise.reject(error);
-    // }
 }
 
 const changePassword = (username, oldPassword, password) => {
@@ -125,27 +48,10 @@ const changePassword = (username, oldPassword, password) => {
         password: oldPassword,
         newPassword: password
     })
-    // const index = mockUsers.findIndex(u => u.username === username);
-    // if (mockUsers[index].password === oldPassword) {
-    //     mockUsers[index].password = password;
-    //     return new Promise((res, rej) => {
-    //         res(userInfoObject(mockUsers[index]));
-    //     })
-    // }
-    // else {
-    //     const error = new Error("Incorrect old password!")
-    //     error.code = 401;
-    //     return Promise.reject(error);
-    // }
 }
 
 const deleteUser = username => {
     return axios.delete(`users/${username}`)
-    // const index = mockUsers.findIndex(user => user.username === username);
-    // mockUsers.splice(index, 1);
-    // return new Promise((res, rej) => {
-    //     res(username);
-    // })
 }
 
 const api = {
